d7683be1c5fe2726bda5c5db8c834eec092664b6
jtschuster <jacksontschuster@gmail.com>
2018-12-08 15:45:26 -0600

    segmented lists that don't work but should

--- mm.c
+++ mm.c
@@ -44,6 +44,10 @@ team_t team = {
 #define WSIZE (SIZE_T_SIZE)
 #define DSIZE (SIZE_T_SIZE*2)
 
+#define NUM_OF_LISTS (13)
+
+#define LIST_ARR_NEXT(p) ((void *)((size_t *)p + 2))
+
 #define CHUNKSIZE (1<<12)
 
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
@@ -66,15 +70,80 @@ team_t team = {
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - SIZE_T_SIZE)))
 #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - SIZE_T_SIZE*2)))
 
+// takes a pointer to a block and gives a ptr to the next block
+#define NEXT(p) ((void *)(*(size_t *)(p)))
+#define PREV(p) ((void *)(*(((size_t *)p + 1))))
+
+// #define NEXT(p) ((void *)(*(size_t *)(p)))
+// #define PREV(p) ((void *)(*((size_t *)((size_t *)p + 1))))
+// takes ptr to a block and changes the ptr stored in the block
+#define SET_NEXT(p, newp) ((size_t *)NEXT(p) = (size_t *)(newp))
+#define SET_PREV(p, newp) ((size_t *)PREV(p) = (size_t *)(newp))
+
+
 #define WSIZE (SIZE_T_SIZE)
 #define DSIZE (SIZE_T_SIZE*2)
 
 void * heap_listp;
+void * list_array_ptr;
+
+void set_next(void* p, void* newp) {
+    (*(size_t *)(p)) = ((size_t *)(newp));
+}
+void set_prev(void* p, void* newp) {
+    if (p) {
+        (*(((size_t *)p + 1))) = ((size_t *)(newp));
+    }
+}
+
+
 
 
 /*
  * mm_init - initialize the malloc package.
  */
+
+
+
+void remove_from_its_list(void* blockp) {
+    size_t size = GET_SIZE(HDRP(blockp));
+    void * listroot = list_array_ptr;
+    for (int i = 0; i< NUM_OF_LISTS - 1; i++){
+        if ((GET_SIZE(HDRP(listroot)) <= size) && (GET_SIZE(HDRP(LIST_ARR_NEXT(listroot)) > size  ))) {
+            break;
+        }
+        listroot = LIST_ARR_NEXT(listroot);
+    }
+    void *elemp = listroot;
+    while (elemp != blockp && elemp) {
+        elemp = NEXT(elemp);
+    }
+    if (elemp) {
+        if (PREV(elemp)){
+        set_next(PREV(elemp), NEXT(elemp));
+        
+        }
+        set_prev(NEXT(elemp), PREV(elemp));
+    }
+    
+}
+
+// make sure bigger than current but smaller than next
+void add_free_block_to_list(void *ptr){
+    size_t size = GET_SIZE(HDRP(ptr));
+    void * list = list_array_ptr;
+    for (int i = 0; i< NUM_OF_LISTS - 1; i++){
+        if ((GET_SIZE(HDRP(list)) <= size) && (GET_SIZE(HDRP(LIST_ARR_NEXT(list))) > size  )) {
+            break;
+        }
+        list = LIST_ARR_NEXT(list);
+    }
+    set_next(ptr, NEXT(list));
+    set_prev(NEXT(ptr), (size_t *)ptr); //will eff up if next of list is null
+    set_next(list, (size_t *)ptr);
+}
+
+
 static void *coalesce(void *bp)
 {
     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
@@ -82,35 +151,48 @@ static void *coalesce(void *bp)
     size_t size = GET_SIZE(HDRP(bp));
 
     if (prev_alloc && next_alloc) { /* Case 1 */  //both are already allocated
+        add_free_block_to_list(bp);
         return bp;
     }
 
     else if (prev_alloc && !next_alloc) { /* Case 2 */ // only prev is allocated, next is free
+        //remove bp from the appropriate list
+        remove_from_its_list(bp);
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size,0));
+        add_free_block_to_list(bp);
     }
 
     else if (!prev_alloc && next_alloc) { /* Case 3 */ //just next is allocated, prev is free
+        remove_from_its_list(bp);
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
         PUT(FTRP(bp), PACK(size, 0));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
+        add_free_block_to_list(bp);
     }
 
     else { /* Case 4 */ //both are free
-        size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
-        GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        remove_from_its_list(bp);
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
+        add_free_block_to_list(bp);
     }
     return bp;
 }
 
+//sets header and footer
+void insert_sizes(void *ptr, size_t size, int alloc) {
+    PUT((void *)HDRP(ptr), PACK(size, alloc));
+    PUT((void *)FTRP(ptr), PACK(size, alloc));
+}
+
 static void *extend_heap(size_t words)
 {
-    char *bp;
+    void *bp;
     size_t size;
 
     /* Allocate an even number of words to maintain alignment */
@@ -119,16 +201,33 @@ static void *extend_heap(size_t words)
         return NULL;
     }
     /* Initialize free block header/footer and the epilogue header */
-    PUT(HDRP(bp), PACK(size, 0)); /* Free block header */
-    PUT(FTRP(bp), PACK(size, 0)); /* Free block footer */
+    insert_sizes((void *)bp, size, 0); /* Free block header */ /* Free block footer */
     PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); /* New epilogue header */
 
     /* Coalesce if the previous block was free */
     return coalesce(bp);
  }
 
+
+void init_array_o_lists() {
+    size_t size = 16;
+    void * base = mem_sbrk((SIZE_T_SIZE+sizeof(void*)) * NUM_OF_LISTS );
+    base = (void*)((size_t *)base + 1);
+    list_array_ptr = base;
+    for (int i = 0; i< NUM_OF_LISTS; i++) {
+        size = size << 1;
+        PUT(HDRP(base), PACK(size, 1));
+        PUT(base, 0);
+        base = LIST_ARR_NEXT(base);
+    }
+}
+
+
 int mm_init(void)
 {
+    init_array_o_lists();
+    
+    
     if ((heap_listp = mem_sbrk(4*SIZE_T_SIZE)) == (void *)-1){
         return -1;
     }
@@ -142,6 +241,9 @@ int mm_init(void)
     if (extend_heap(CHUNKSIZE/SIZE_T_SIZE) == NULL){
         return -1;
     }
+
+    //add_free_block_to_list(heap_listp);
+
     return 0;
 }
 
@@ -189,6 +291,31 @@ static void place(void *bp, size_t asize)
     }
 }
 
+//returns pointer to root of list of right size
+void* find_block_that_fits(size_t size){
+    void * list = list_array_ptr;
+    for (int i = 0; i < NUM_OF_LISTS - 1; i++){
+        if ((GET_SIZE(HDRP(list)) > size) && (NEXT(list) != NULL)) {
+            return list;
+        }
+        list = LIST_ARR_NEXT(list);
+    }
+    return NULL;
+}
+
+void break_block(void * ptr, size_t size) {
+    size_t old_size = GET_SIZE(HDRP(ptr));
+    size_t rest_size = old_size - size;
+    if (rest_size > 4*SIZE_T_SIZE) {
+        insert_sizes(ptr, size, 1);
+        void * rest_ptr = NEXT_BLKP(ptr);
+        insert_sizes(rest_ptr, rest_size, 0);
+        add_free_block_to_list(rest_ptr);
+    } 
+    else{
+        insert_sizes(ptr, size, 1);
+    }
+}
 
 void *mm_malloc(size_t size) // implicit list with size_t_size bytes indicating size at start
 {
@@ -197,26 +324,28 @@ void *mm_malloc(size_t size) // implicit list with size_t_size bytes indicating 
     char *bp;
 
     /* Ignore spurious requests */
-    if (size == 0)
+    if (size == 0) {
         return NULL;
-
+    }
     /* Adjust block size to include overhead and alignment reqs. */
-    if (size <= DSIZE)
+    if (size <= DSIZE){
         asize = 2*DSIZE;
-    else
+    }
+    else {
         asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
-
+    }
     /* Search the free list for a fit */
-    if ((bp = find_fit(asize)) != NULL) {
-        place(bp, asize);
+    if ((bp = find_block_that_fits(asize)) != NULL) {
+        break_block(bp, asize); //break block
         return bp;
     }
 
     /* No fit found. Get more memory and place the block */
     extendsize = MAX(asize,CHUNKSIZE);
-    if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
+    if ((bp = extend_heap(extendsize/WSIZE)) == NULL){
         return NULL;
-    place(bp, asize);
+    }
+    break_block(bp, asize);
     return bp;
 }
 
